4b3a03da865674d74ea7b7842108673a6a526ce4
Kumail <kumailhabib12@gmail.com>
2020-08-04 21:53:52 +0500

    Added maxHeap

---
+++ Data Structures/heaps/maxHeap.h
@@ -0,0 +1,6 @@
+void buildMaxHeap(int data[], int size);
+void maxHeapify(int data[], int i, int n);
+void printMaxHeap(int data[], int heapSize);
+int peekMax(int heap[], int heapSize);
+int extractMax(int heap[], int n, int* heapSize);
+int getParent(int i);
