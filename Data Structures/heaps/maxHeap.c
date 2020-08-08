fd2b878d27ccfb7d1b6725c0a4ab4c00a037bb57
Kumail <kumailhabib12@gmail.com>
2020-08-08 20:38:04 +0500

    Updated maxHeap

--- Data Structures/heaps/maxHeap.c
+++ Data Structures/heaps/maxHeap.c
@@ -79,14 +79,6 @@ int extractMax(int heap[], int n, int* heapSize)
     return maxElement;
 }
 
-// add new value in the heap
-void addElement(int data[], int n, int value, int* heapSize)
-{
-    // increase the size of heap 
-    *heapSize = *heapSize + 1;
-    data[n - 1] = value;
-    maxHeapify(data, n - 1, n);
-}
 
 // returns the first element as maximum element 
 int peekMax(int heap[], int heapSize)
--- Data Structures/heaps/maxHeap.h
+++ Data Structures/heaps/maxHeap.h
@@ -4,5 +4,3 @@ void printMaxHeap(int data[], int heapSize);
 int peekMax(int heap[], int heapSize);
 int extractMax(int heap[], int n, int* heapSize);
 int getParent(int i);
-void addElement(int data[], int n, int value, int* heapSize);
-
