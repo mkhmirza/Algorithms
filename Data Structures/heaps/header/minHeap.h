df330f336d337d59f4f15094faaa45485ef5b030
Kumail <kumailhabib12@gmail.com>
2020-08-13 21:35:39 +0500

    added decrease key

--- Data Structures/heaps/header/minHeap.h
+++ Data Structures/heaps/header/minHeap.h
@@ -4,3 +4,4 @@ void printMinHeap(int data[], int heapSize);
 int peekMin(int heap[], int heapSize);
 int extractMin(int heap[], int n, int* heapSize);
 int getParentMin(int i);
+int decreaseKey(int heap[], int index, int newValue);
