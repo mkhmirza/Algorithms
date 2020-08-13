5b2fd326658fd38fa80438172b8ff24c18608d6a
Kumail <kumailhabib12@gmail.com>
2020-08-13 22:33:10 +0500

    node representation for binary search tree

---
+++ Data Structures/trees/bst/Node.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace  bst
+{
+    public class Node
+    {
+        public int Key {get; set;}
+        public Node Left{get; set;}
+        public Node Right{get; set;}
+        public Node Parent{get; set;}
+    }
+
+}
