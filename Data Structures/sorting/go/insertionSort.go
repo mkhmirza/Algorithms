542f422fb6e5d7253108c986895e8a60ea2375d6
Kumail <kumailhabib12@gmail.com>
2020-08-13 19:59:28 +0500

    added insertion sort

---
+++ Data Structures/sorting/go/insertionSort.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+// sorts data array using insertion sort n ascending order 
+func insertionSortAcending(array [10]int) []int {
+	var i int = 0 // counter 
+	for i < len(array){
+		j := i
+		for j > 0 {
+			if array[j] < array[j - 1]{
+				// swap values 
+				array[j], array[j-1] = array[j-1], array[j]
+			}
+			j--
+		}
+		i++
+	}
+
+	return array[:10]
+}
+
+func insertionSortDescending(array [10]int) []int {
+	var i int = 0 // counter 
+	for i < len(array){
+		j := i
+		for j > 0 {
+			if array[j] > array[j - 1]{
+				// swap values 
+				array[j], array[j-1] = array[j-1], array[j]
+			}
+			j--
+		}
+		i++
+	}
+
+	return array[:10]
+}
+
+func main(){
+
+	fmt.Println("Program to demostrate working of insertion sort")
+	array := [10]int{1, 4, 5, 0, 99, 67, 1, 100, 2, 8}
+	sortedArrayAscending := insertionSortAcending(array)
+	
+	fmt.Println("Original Array: ")
+	fmt.Println(array)
+	fmt.Println("Sorted Array Acending Order: ")
+	fmt.Println(sortedArrayAscending)
+	
+	fmt.Println()
+
+	sortedArrayDescending := insertionSortDescending(array)
+	fmt.Println("Original Array: ")
+	fmt.Println(array)
+	fmt.Println("Sorted Array Descending Order: ")
+	fmt.Println(sortedArrayDescending)
+}
