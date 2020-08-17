3dce3855061a941e242205393264e051a61d18ce
Kumail <kumailhabib12@gmail.com>
2020-08-18 01:47:06 +0500

    sorting algorithms having worst case O(n)

---
+++ sorting/linear/counting.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python env
+
+# program for sorting data using counting sort
+
+a = [1 , 2, 4, 5, 6, 0, 2]
+k = max(a)
+c = [0 for x in range(k+1)]
+
+# calculate frequency/count
+for i in range(0,k+1):
+    c[a[i]] += 1
+# calculate cummulative frequency
+for i in range(0,len(a)):
+    c[i] += c[i-1]
+
+
+b = [0] * len(a)
+i = len(a) - 1
+# perform actual sorting
+while i >= 0:
+    b[c[a[i]] - 2] = a[i]
+    c[a[i]] -= 1
+    i -= 1  
+
+print(f"Original Array: {a}")
+print(f"Sorted Data:    {b}")
