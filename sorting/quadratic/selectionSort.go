d3850e4a701c41a98281f2f06b97b9e4135dc5a0
Kumail <kumailhabib12@gmail.com>
2020-08-19 01:11:49 +0500

    added selection sort

---
+++ sorting/quadratic/selectionSort.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func selectionSort(array []int) []int{
+
+	for i:=0; i < len(array); i++{
+		minElement := i
+
+		for j:=0; j < len(array); j++{
+			if array[j] > array[minElement]{
+				minElement = j
+			}
+		}
+
+		if minElement != i{
+			array[i], array[minElement] = array[minElement], array[i]
+		}
+	}
+	return array[:len(array)]
+}
+
+
+
+func main(){
+	fmt.Println("Program demostration for working of selection sort")
+	array := []int {64, 11, 22, 25, 64, 12}
+	fmt.Println("Original Array: ")
+	fmt.Println(array)
+	sortedArray := selectionSort(array)
+	
+	fmt.Println("Sorted Array: ")
+	fmt.Println(sortedArray)
+}
